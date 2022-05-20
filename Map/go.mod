eb68ccf575e1c8a6735452c5850b8fa398a19aee
liuzhixin405 <164910441@qq.com>
2022-05-20 22:36:14 +0800

    u
    u

---
+++ Map/go.mod
@@ -0,0 +1,3 @@
+module map_test.go
+
+go 1.18
---
+++ Map/map_test.go
@@ -0,0 +1,44 @@
+package map_ext
+
+import "testing"
+
+func TestMapWithFunValue(t *testing.T) {
+	m := map[int]func(op int) int{}
+
+	m[1] = func(op int) int { return op }
+	m[2] = func(op int) int { return op * op }
+	m[3] = func(op int) int { return op * op * op }
+	t.Log(m[1](2), m[2](2), m[3](2))
+}
+
+func TestMapForSet(t *testing.T) {
+	mySet := map[int]bool{}
+
+	mySet[1] = true
+	n := 3
+	if mySet[n] {
+		t.Logf("%d is existing", n)
+	} else {
+		t.Logf("%d is not existing", n)
+	}
+	mySet[3] = false
+	t.Log(len(mySet))
+	delete(mySet, 1)
+	n = 1
+	if mySet[n] {
+		t.Logf("%d is existing", n)
+	} else {
+		t.Logf("%d is not existing", n)
+	}
+
+}
+
+// go test -run TestMapWithFunValue  -v
+
+/*
+=== RUN   TestMapWithFunValue
+    map_test.go:11: 2 4 8
+--- PASS: TestMapWithFunValue (0.00s)
+PASS
+ok      map_test.go     0.185s
+*/
