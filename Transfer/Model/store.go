03dc3b84fcb6696f961bb8bca8de8e4bd01b91c7
liuzhixin405 <164910441@qq.com>
2022-06-05 13:16:27 +0800

    u
    u

--- Transfer/Model/store.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "github.com/shopspring/decimal"
-
-type Store interface {
-	TransferAssets(token, orderId, coin string, amount decimal.Decimal, side TransferDirection) bool
-	ConfirmTransfer(orderId string, success bool) bool
-	GetCoinAvailableQuantity(customerId string, coin string) decimal.Decimal
-}
