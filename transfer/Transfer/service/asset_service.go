3d07d0a690814cc10717a3225cce4cf4d4c44e73
liuzhixin405 <164910441@qq.com>
2023-08-15 16:04:16 +0800

    update

--- transfer/Transfer/service/asset_service.go
+++ transfer/Transfer/service/asset_service.go
@@ -2,18 +2,20 @@ package service
 
 import (
 	"transferasset/model"
+	"transferasset/model/psql"
 
 	"github.com/shopspring/decimal"
 )
 
 func TransferAssets(token, orderId, coin string, amount decimal.Decimal, side model.TransferDirection) bool {
-	return true
+	return psql.SharedStore().TransferAssets(token, orderId, coin, amount, side)
 }
 
 func ConfirmTransfer(orderId string, success bool) bool {
-	return true
+	return psql.SharedStore().ConfirmTransfer(orderId, success)
 }
 
 func GetCoinAvailableQuantity(customerId string, coin string) decimal.Decimal {
-	return decimal.NewFromInt32(123)
+	//return decimal.NewFromInt32(123)
+	return psql.SharedStore().GetCoinAvailableQuantity(customerId, coin)
 }
